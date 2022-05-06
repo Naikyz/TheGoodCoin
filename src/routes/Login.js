cb02418fac4b44aa69c968a575e8450f983476c4
Naikyz <kylian.paulin@epitech.eu>
2022-05-06 20:25:13 +0200

    add: api interaction on login button

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+
 function Login() {
+    const axios = require("axios");
     return (
         <div>
             <h1>Login</h1>
+            <button onClick={() => {
+                const http = axios.create({
+                    baseURL: "https://api.starton.io/v2",
+                    headers: {
+                        "x-api-key": 'VN1V84XJ2undibI57qrvFpKSnMlNFDta',
+                    },
+                })
+                http.get('/smart-contract-template').then(response => {
+                console.log(response.data)
+                })
+            }
+}>CALL API</button>
         </div>
     );
 }
