944cf907cc09f43fc51095e10a70c6926cfc0dd4
ismael FALL <ismael.fall@epitech.eu>
2022-05-07 00:03:13 +0200

    add 'db'

---
+++ src/backend/productType.py
@@ -0,0 +1,8 @@
+
+
+class Product():
+    def __init__(self, address, product, product_address, price):
+        self.address = address
+        self.product = product
+        self.product_address = product_address
+        self.price = price
