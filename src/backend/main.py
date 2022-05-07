63788449848a8a67bede344ebf4a5f5af381b619
ismael FALL <ismael.fall@epitech.eu>
2022-05-07 19:25:02 +0200

    add: backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from productType import Product
 from json import dumps, loads
 from os.path import exists
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,13 +11,23 @@ headers = {
     'Content-Type': 'application/json'
 }
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/watcher/")
+@app.post("/CID/")
 async def create_watcher(params: dict):
     if exists('save.json'):
         f = open("./save.json", "r")
@@ -24,9 +35,7 @@ async def create_watcher(params: dict):
         f.close()
     else:
         data = []
-    data.append(
-        {"address": params["address"], "product": params["product"], "product_address": params["product_address"],
-         "price": params["price"]})
+    data.append({"name": params["name"], "description": params["description"],"price": params["price"], "cid": params["cid"]})
     print(data)
     f = open("./save.json", "w")
     f.write(dumps(data))
@@ -34,7 +43,7 @@ async def create_watcher(params: dict):
     return 200
 
 
-@app.get("/getallwatcher/")
+@app.get("/CID/")
 async def get_watchers():
     if exists('save.json'):
         f = open("./save.json", "r")
@@ -43,3 +52,18 @@ async def get_watchers():
     else:
         data = []
     return data
+
+
+@app.delete("/CID/")
+async def delete_watcher(params: dict):
+    if exists('save.json'):
+        f = open("./save.json", "r")
+        data = loads(f.read())
+        f.close()
+    else:
+        data = []
+    data.remove(params["CID"])
+    f = open("./save.json", "w")
+    f.write(dumps(data))
+    f.close()
+    return 200
