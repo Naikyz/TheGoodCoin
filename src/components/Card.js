aaed62e605f156c29121d635970a092ec65ad8e4
Naikyz <kylian.paulin@epitech.eu>
2022-05-08 17:42:54 +0200

    add: good version

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import React from "react";
 import axios from 'axios';
-import Modal from "./Modal";
-axios.defaults.headers['x-api-key'] = '8RT7VQVZUUCux2vbf1Ng0utDldWU6QJo';
+// require('dotenv').config()
+// import express from 'express'
+axios.defaults.headers['x-api-key'] = process.env.REACT_APP_API_KEY;
 
 
-export default function ItemCard({ item, reload, owned, sold }) {
+export default function ItemCard({item, reload, owned, sold}) {
 
     const [bought, setBought] = useState(false);
     // console.log(process.env.API_KEY)
@@ -62,31 +63,24 @@ export default function ItemCard({ item, reload, owned, sold }) {
     }
 
     return (
-        <>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg mt-12">
-                <img className="w-full" src={"https://ipfs.io/ipfs/" + item.CID} alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{item.name}</div>
-                    <p className="text-gray-700 text-base">
-                        {item.description}
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    {owned === false && sold !== true && <button onClick={handleOnClick} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 ">{item.price} $MATIC</button>}
-                    {owned === true && <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Buyed</div>}
-                    {sold === true && <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Sold</div>}
-                </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg mt-12">
+            <img className="w-full" src={"https://ipfs.io/ipfs/" + item.CID} alt="Sunset in the mountains"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{item.name}</div>
+                <p className="text-gray-700 text-base">
+                {item.description}
+                </p>
             </div>
             <div className="px-6 pt-4 pb-2">
                 { owned === false && sold !== true && <button onClick={mintNft} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{item.price} $MATIC</button>}
                 { owned === true && <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Bought</div>}
                 { sold === true && <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Sold</div>}
             </div>
-        </>
+        </div>
     );
 
 
-
+    
     // return (
     //     <div style={{width:"350px"}}>
     //     <Card className="bg-black">
