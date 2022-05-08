c0d8b11754b79b5e26a89aa3109bbe0431f744c7
Mr.Slooth <victor.guyot@epitech.eu>
2022-05-08 12:23:37 +0200

    feat: profile list element

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,24 @@
-import React, {useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import Footer from "../components/Footer";
 import { Jazzicon } from '@ukstv/jazzicon-react';
 import styled from '@emotion/styled';
+import axios from 'axios';
+import ItemCard from "../components/Card";
+axios.defaults.headers['x-api-key'] = '8RT7VQVZUUCux2vbf1Ng0utDldWU6QJo';
 
 
 function Profile() {
+    const starton = axios.create({
+        baseURL: "https://api.starton.io/v2",
+    });
+    const [data, setData] = useState([{}]);
+
+    
+    useEffect(() => {
+        let account = localStorage.getItem('account');
+        setData(starton.get("https://aleph.sh/vm/d21949b4839ac48766cfa68c7b73eb88f31362bddf8857a096108c2ec4bc55b7/sales/owner/" + account));
+    }, [starton])
+
     let account = localStorage.getItem('account');
     console.log(account);
     const ModifiedJazzicon = styled(Jazzicon)({
@@ -45,6 +59,17 @@ function Profile() {
                         </div>
                     </div>
                 </div>
+                <>
+                <div>
+                {data.length ?
+                <div className="flex flex-wrap justify-around content-center">
+                    {data.data.map((item) => (<ItemCard key={item.CID} item={item}/>))}
+                </div> : <>Chargement ...</>}
+                </div>
+                    <footer className="bg-white">  
+                        <Footer />
+                    </footer>
+                </>
 
             </div>
             <footer className="bg-white">
