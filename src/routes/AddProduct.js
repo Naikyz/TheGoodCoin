0b3d2af39a25bf12556cd64ec1c4d521a9dc0786
Naikyz <kylian.paulin@epitech.eu>
2022-05-08 16:28:04 +0200

    delete required

--- src/routes/AddProduct.js
+++ src/routes/AddProduct.js
@@ -67,24 +67,27 @@ function AddProduct() {
                 <h1 className="block text-gray-700 text-center font-bold mb-2" >Enter informations about your product</h1>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2">Name*: </label>
-                    <input required="required" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Name of your product" onChange={(event) => setName({value: event.target.value})} />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Name of your product" onChange={(event) => setName({value: event.target.value})} />
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-800 text-sm font-bold mb-2">Description*: </label>
-                    <input required="required" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Description of your product" onChange={(event) => setDescription({value: event.target.value})} />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Description of your product" onChange={(event) => setDescription({value: event.target.value})} />
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-800 text-sm font-bold mb-2">Price ($MATIC)*: </label>
-                    <input required="required" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="0" onChange={(event) => setPrice({value: event.target.value})} />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="0" onChange={(event) => setPrice({value: event.target.value})} />
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-800 text-sm font-bold mb-2">Image of your product*: </label>
-                    <input required="required" className="pt-6 pb-8 mb-4 border rounded p-10" type="file" name="image" onChange={uploadPicture} />
+                    <input className="pt-6 pb-8 mb-4 border rounded p-10" type="file" name="image" onChange={uploadPicture} />
                 </div>
                 <div className="mb-4">
                 <button onClick={handleOnClick} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" type="submit"  name="upload">
                         Upload
                     </button> 
+                </div>
+                <div className="mb-4">
+                <   p className="block text-gray-700 text-center font-bold mb-2" >* = Needed section</p>
                 </div>
             </form>
         </div>
